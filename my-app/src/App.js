b2be01215f087f64c1c3a54759e1a12e4781a512
CesarJSmokowski <cesar8800@vt.edu>
2023-05-03 16:57:42 -0400

    App.js Changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -415,6 +415,63 @@ function NewTask(props){
     })
   }
 
+  function chatGPT() {
+    console.log("chatGPT Called");
+
+    const { Configuration, OpenAIApi } = require("openai");
+
+    const configuration = new Configuration({
+      //apiKey: process.env.OPENAI_API_KEY,
+      apiKey: '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',
+    });
+    const openai = new OpenAIApi(configuration);
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Summarize this for a second-grade student:\n\nJupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus.",
+      temperature: 0.7,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    });
+
+    /*
+    const apiKey = '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';
+    const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+
+    const prompt = 'Hello, ChatGPT!';
+    const maxTokens = 10;
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+        max_tokens: maxTokens
+      })
+    };
+
+    fetch(apiUrl, requestOptions)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data.choices[0].text);
+      })
+      .catch(error => {
+        console.error('There was a problem with the Chat GPT API request:', error);
+      });
+      */
+
+  }
+
   return (
     <tr className = "border">
       <td className="text-center" style={{verticalAlign: "middle"}}>
@@ -427,6 +484,9 @@ function NewTask(props){
       <td className="text-end" colSpan = "2">
         <button type="button" className="btn btn-default btn-success" alt="Add New Task" onClick = {addTask}>
           <i className="bi bi-plus-square"></i> Add New Task
+        </button>
+        <button type="button" className="btn btn-default btn-success" alt="Chat GPT" onClick = {chatGPT}>
+          <i className="bi bi-plus-square"></i> Chat GPT
         </button>
       </td>
     </tr>
